f5f668d5259e069c81bfbc41427bfaf247d863a0
rafik <rafik@rafik-Aspire-5742>
2012-11-28 02:34:48 -0200

    apaguei quase tudo

--- src/br/com/sp/beans/Estado.java
+++ src/br/com/sp/beans/Estado.java
@@ -3,10 +3,6 @@ package br.com.sp.beans;
 public class Estado {
 	
 	private String nome_estado;
-	
-	private String sigla;
-	
-	private String novoTeste;
 
 	public String getNome_estado() {
 		return nome_estado;
